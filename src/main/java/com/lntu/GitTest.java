925f03bb0b66de79da47543d7e8890cbe3b3fce8
waterbottel <waterbottel@qq.com>
2021-11-02 10:58:16 +0800

    first commit for testing commit function supplied by git which is integrated by IDEA. A maven project will be committed , and it's meaningless , just for test. Time:202111021053.

---
+++ src/main/java/com/lntu/GitTest.java
@@ -0,0 +1,10 @@
+package com.lntu;
+
+/**
+ * @author watercup
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Idea_maven_git!");
+    }
+}
