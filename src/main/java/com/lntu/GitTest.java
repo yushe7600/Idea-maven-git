9ea2ab227a4a873c54d37499eff3867e5641ec18
waterbottel <waterbottel@qq.com>
2021-11-02 11:00:05 +0800

    second commit. Time:202111021100

--- src/main/java/com/lntu/GitTest.java
+++ src/main/java/com/lntu/GitTest.java
@@ -6,5 +6,6 @@ package com.lntu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Idea_maven_git!");
+        System.out.println("hello Idea_maven_git2!");
     }
 }
